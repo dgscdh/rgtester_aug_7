56e4729aff5a9e6e8eb4f4eaf1e43938ae7a8f9b
dgscdh <44707414+dgscdh@users.noreply.github.com>
2020-08-07 13:31:02 -0500

    Update HW.java

--- HW.java
+++ HW.java
@@ -1,5 +1,5 @@
 public class HW {
     public static void main(String[] args) {
-        System.out.println("Hello World! This is a new version.");
+        System.out.println("Hello World! This is a new version edited on Github.");
     }
 }
