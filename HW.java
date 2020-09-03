007dd0841841dc150e4d4df1c9a6ae7adf967731
dgsobolewski <dgsobolewski@gmail.com>
2020-09-03 08:56:26 -0500

    Update HW.java

--- HW.java
+++ HW.java
@@ -2,5 +2,6 @@ public class HW {
     public static void main(String[] args) {
         System.out.println("This version was updated at about 8:47 on 9-3.");
         System.out.println("This line was then added at about 8:49 on 9-3.");
+        System.out.println("This line was then added at about 8:56 on 9-3.");
     }
 }
