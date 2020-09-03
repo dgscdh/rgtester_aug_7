439e74437e899ec084b9a0ed579a7e736a1b4abd
dgsobolewski <dgsobolewski@gmail.com>
2020-09-03 08:47:40 -0500

    Update HW.java

--- HW.java
+++ HW.java
@@ -1,9 +1,5 @@
 public class HW {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World! This is another new version. BLAH BLAH BLAH");
-=======
-        System.out.println("Hello World! This is a new version edited on Github.");
->>>>>>> origin/master
+        System.out.println("This version was updated at about 8:47 on 9-3.");
     }
 }
